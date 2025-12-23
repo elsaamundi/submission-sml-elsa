262937fc46e26f10da0325ceeec644e5f6b68f31
Elsa <limechild4@gmail.com>
2025-12-23 15:35:41 +0700

    Fix MLflow experiment conflict

--- Workflow-CI/MLProject/modelling.py
+++ Workflow-CI/MLProject/modelling.py
@@ -6,37 +6,34 @@ import mlflow
 import mlflow.sklearn
 
 def main():
-    # 1. Load Data
-    # Path ini sudah benar untuk Docker (Folder data ada di sebelah script)
-    # Pastikan kamu sudah copy folder 'telco_churn_preprocessing' ke dalam 'Workflow-CI/MLProject'
+    # Load Data (Path ini udah bener buat Docker)
     data_path = 'telco_churn_preprocessing/train_processed.csv'
     
-    # Cek error handling biar ketahuan kalau data lupa dicopy
     try:
         df = pd.read_csv(data_path)
     except FileNotFoundError:
-        print(f"ERROR: File tidak ditemukan di {data_path}. Pastikan folder dataset sudah dicopy ke dalam Workflow-CI/MLProject.")
+        print(f"ERROR: File tidak ditemukan di {data_path}")
         return
 
     X = df.drop('Churn', axis=1)
     y = df['Churn']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # 2. Setup MLflow LOKAL (Tanpa DagsHub)
-    mlflow.set_experiment("Eksperimen_CI_Docker")
-    mlflow.sklearn.autolog() # Sesuai permintaan reviewer
+    # --- BAGIAN INI DIHAPUS BIAR GAK CRASH DI GITHUB ---
+    # mlflow.set_experiment("Eksperimen_CI_Docker") 
+    # ---------------------------------------------------
 
-    # 3. Training
+    mlflow.sklearn.autolog()
+
+    # start_run() akan otomatis pakai eksperimen yang disiapin GitHub Action
     with mlflow.start_run():
         model = RandomForestClassifier(n_estimators=100, random_state=42)
         model.fit(X_train, y_train)
 
-        # Evaluasi
         predictions = model.predict(X_test)
         acc = accuracy_score(y_test, predictions)
         print(f"Model Accuracy: {acc}")
         
-        # Simpan Model (Penting biar 'MLflow Run' dianggap valid)
         mlflow.sklearn.log_model(model, "model", registered_model_name="model_random_forest")
 
 if __name__ == "__main__":
